9ff37eef704d10cba1982c6d1cf17613be47ac83
Francis <francismitra@gmail.com>
2020-07-28 16:03:45 -0400

    Add header

---
+++ src/login/components/DeviceHeader.tsx
@@ -0,0 +1,13 @@
+import { h } from 'preact';
+
+const DeviceHeader = (): JSX.Element => (
+  <div class='code-header'>
+    <div class='code-header__content'>
+      <div class='code-header__title'>
+        <span>Almost there!</span>
+      </div>
+    </div>
+  </div>
+);
+
+export default DeviceHeader;
